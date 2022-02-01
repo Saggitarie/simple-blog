8808cb15f2ccaf39c7e84f6c999894ad31ef6c92
Kohki Shiga <shiga@werks.co.jp>
2022-02-01 12:42:48 +0900

    Commit Rest of the necessary files

---
+++ src/components/button.tsx
@@ -0,0 +1,33 @@
+import "@components/button.sass";
+
+import { RootState } from "@app/store";
+import { useSelector } from "react-redux";
+
+import "@components/button.sass";
+
+type pageNavigationFunctionType = (direction: string) => void;
+
+type Props = {
+  direction: string;
+  handleEvent: pageNavigationFunctionType;
+  children: React.ReactNode;
+};
+
+const Button: React.FC<Props> = ({ direction, handleEvent, children }) => {
+  const currentIndex = useSelector((state: RootState) => state.posts.paginationIndex);
+  const endIndex = useSelector((state: RootState) => state.posts.paginationEndIndex);
+
+  const isClickDisabled =
+    (direction === "next" && currentIndex === endIndex) ||
+    (direction === "prev" && currentIndex === 0);
+
+  return (
+    <div id="button">
+      <button onClick={() => handleEvent(direction)} disabled={isClickDisabled}>
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
